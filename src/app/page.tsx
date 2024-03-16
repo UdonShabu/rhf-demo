57da853fae96be2d4641234db7dcfa8db24b4362
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-03-16 21:38:28 +0700

    Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { YoutubeForm } from "@/components/youtube-form";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center mt-10">
+      <YoutubeForm/>
+    </main>
+   
+  );
+}
