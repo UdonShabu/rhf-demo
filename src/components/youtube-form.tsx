bb7eb41ad747563c1a093ddbd10d6df06407f1fb
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-03-17 13:09:49 +0700

    3S: Submit data

--- src/components/youtube-form.tsx
+++ src/components/youtube-form.tsx
@@ -3,29 +3,33 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { DevTool } from '@hookform/devtools'
 
+type FormValues = {
+  username: string
+  email: string
+  channel: string
+}
 
 export const YoutubeForm = () => {
-    const form = useForm()
-    const {register, control} = form
+    const form = useForm<FormValues>()
+    const {register, control, handleSubmit} = form
 
-    const onSubmit = () => {
-      // TODO: How can u submit and log data out with correct a type?
-      console.log('Form submitted')
+    const onSubmit = (data: FormValues) => {
+      console.log('Form submitted', data)
     }
 
   return (
     <div>
-        <form onSubmit={}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor="username">Username</label>
-            <input type="text" id='username' {...register('username')} />
+            <input type="text" id='username' {...register('username')} className='bg-slate-600'/>
 
             <label htmlFor="email">Email</label>
-            <input type="email" id='email' {...register('email')} />
+            <input type="email" id='email' {...register('email')}  className='bg-slate-600'/>
 
             <label htmlFor="channel">Channel</label>
-            <input type="text" id='channel' {...register('channel')}/>
+            <input type="text" id='channel' {...register('channel')} className='bg-slate-600'/>
 
-            <button >Submit</button>
+            <button className='rounded-md bg-sky-600 px-3 py-2 mt-2'>Submit</button>
         </form>
         <DevTool control={control}/>
     </div>
