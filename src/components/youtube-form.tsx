0ee861b2eca4757c2f5be67017da08b68ed208b0
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-03-17 12:52:01 +0700

    2S: Connect fields

--- src/components/youtube-form.tsx
+++ src/components/youtube-form.tsx
@@ -6,20 +6,18 @@ import { useForm } from 'react-hook-form'
 export const YoutubeForm = () => {
     const form = useForm()
     const {register} = form
-    // TODO: Connect this to each field with a short way
-    const {name, ref, onBlur, onChange} = register('username')
 
   return (
     <div>
         <form action="">
             <label htmlFor="username">Username</label>
-            <input type="text" id='username' name='username' />
+            <input type="text" id='username' {...register('username')} />
 
             <label htmlFor="email">Email</label>
-            <input type="email" id='email' name='email' />
+            <input type="email" id='email' {...register('email')} />
 
             <label htmlFor="channel">Channel</label>
-            <input type="text" id='channel' name='channel' />
+            <input type="text" id='channel' {...register('channel')}/>
 
             <button >Submit</button>
         </form>
